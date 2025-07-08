62b5c53e5e4dfee1b594d2068958830e8ed46a68
Irfan S <irfanspangodu@gmail.com>
2025-07-09 02:03:31 +0530

    created arrival

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    // Initialize the banner slider
+    
     $('.banner').slick({
         autoplay: true,
         autoplaySpeed: 3000,
@@ -13,10 +15,49 @@ $(document).ready(function () {
 
         prevArrow: '<button type="button" class="slick-prev custom-arrow"><img src="assets/icons/left-arrow-bold.svg" alt="prev-icon" /></button>',
         nextArrow: '<button type="button" class="slick-next custom-arrow"><img src="assets/icons/right-arrow-bold.svg" alt="next-icon" /><</button>',
-        
+
         // Custom Paging for horizontal rectangular indicators
         customPaging: function (slider, i) {
             return '<button type="button" class="horizontal-indicator"></button>';
         }
     });
+
+
+    // Initialize the arrival section slider
+
+    $('.arrival').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev custom-arrow"><img src="assets/icons/arrow-left.svg" alt="prev-icon" /></button>',
+        nextArrow: '<button type="button" class="slick-next custom-arrow"><img src="assets/icons/arrow-right.svg" alt="next-icon" /></button>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    });
+
 });
